9a6ebecf8756093638ce89ce1fa8231746691a1e
Murali Shanmugham <murlionline@gmail.com>
2018-04-30 19:27:02 +1000

    Update app.controller.js

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -198,7 +198,7 @@ sap.ui.define([
 		// Step 4C: Implementation for Edit Button press event. This function will enable to the decision table to be edited
 		onEditPress: function() {
 			var ruleBuilderId = this.getView().byId("ruleBuilder");
-			ruleBuilderId.setEditable(true);
+			
 
 			this.getView().byId("cancelButton").setVisible(true);
 			this.getView().byId("deployButton").setVisible(true);
@@ -212,6 +212,7 @@ sap.ui.define([
 					var oRuleModel = ruleBuilderId.getModel();
 					this.setResultDataObject();
 					oRuleModel.refresh(true);
+					ruleBuilderId.setEditable(true);
 				}.bind(this)
 			};
 			this.handleActionPress(oFIParam);
